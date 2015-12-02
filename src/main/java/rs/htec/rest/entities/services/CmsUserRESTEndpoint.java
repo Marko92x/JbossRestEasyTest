09b8dcfe46ad2510c54dbc8ac4fecd6c2148b4ae
marko92x <mdbarackov@gmail.com>
2015-12-02 16:34:58 +0100

    services

---
+++ src/main/java/rs/htec/rest/entities/services/CmsUserRESTEndpoint.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.htec.rest.entities.services;
+
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author marko
+ */
+@Path("/cmsUsers")
+public class CmsUserRESTEndpoint {
+    
+}
