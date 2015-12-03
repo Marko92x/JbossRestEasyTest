f8b138315a469996523af9a5b5a8923f0b509887
marko92x <mdbarackov@gmail.com>
2015-12-03 11:11:23 +0100

    services

---
+++ src/main/java/rs/htec/rest/entities/services/CmsRoleRESTEndpoint.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.htec.rest.entities.services;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author marko
+ */
+@Stateless
+@Path("/roles")
+public class CmsRoleRESTEndpoint {
+    
+    @PersistenceContext
+    private EntityManager em;
+}
